58bfb135f88f886965b365e80c3f6c6a0e7aecda
BugHunter-X <askzen01@gmail.com>
2024-12-01 23:20:12 +0100

    Add bug.cpp file

---
+++ bug.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+int main() {
+  std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+  for (int i = 0; i <= numbers.size(); ++i) {
+    std::cout << numbers[i] << std::endl;
+  }
+
+  return 0;
